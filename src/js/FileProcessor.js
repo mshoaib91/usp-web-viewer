f3214ae099c2f9150ae53b1d7c9fa3a4b60d0f93
Shoaib Khan <mshoaibkhan91@gmail.com>
2018-07-07 21:41:40 +0200

    jszip added

--- src/js/FileProcessor.js
+++ src/js/FileProcessor.js
@@ -1,5 +1,7 @@
 /** created by shoaib khan on 07.07.2018 */
 
+import JSZip from 'jszip';
+
 /**
  * This class processes the uploaded zip file,
  * extract the obj file from it and return it
@@ -9,8 +11,12 @@ class FileProcessor {
   readZip(zipFile) {
     this.readFile(zipFile)
     .then((buffer) => {
-      console.log('buffer of the file');
-      console.log(buffer);
+      let zip = new JSZip();
+      return zip.loadAsync(buffer)
+    })
+    .then((zip) => {
+      console.log('response from jszip');
+      console.log(zip);
     })
     .catch(err => {
       console.error(err);
